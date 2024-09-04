96a38b9dc063021170110548369553a1aeb0e878
Furhan Ahmed <itz.farhan6710@gmail.com>
2024-09-05 05:13:29 +0530

    Redux-implemented-and-solved-errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import ClientLayout from "./ClientLayout"; // Import the client component
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "Your Custom Title",
+  description: "Hakathon Organiser App UI",
+  icons: {
+    icon: "/images/favicon.ico", // Path to your favicon
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
